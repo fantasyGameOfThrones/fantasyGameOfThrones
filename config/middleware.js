32d6b7ff37ecfae777142fb109b1290e6d67432f
Brian Graham <graham.briank@gmail.com>
2015-12-18 15:28:38 -0800

    modify gitignore to remove .py files, move all dist files into dist folder, ignore bundle.js

--- config/middleware.js
+++ config/middleware.js
@@ -8,7 +8,7 @@ module.exports = function (app, express) {
 
   app.use(bodyParser.urlencoded({extended : true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../client'));
+  app.use(express.static(__dirname + '/../dist'));
 
   app.use('/api/users', userRouter);
   app.use('/api/leagues', leagueRouter);
