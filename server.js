7f0eb9907fcf5557b66fd0f80282fde443e096a9
Brian Graham <graham.briank@gmail.com>
2015-12-18 16:33:10 -0800

    Add server error handling

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var bodyParse = require('body-parser');
 
 var app = express();
 
 require('./config/middleware')(app, express); 
 
-app.listen(8000, function () {
+app.listen(process.env.PORT || 8000, function () {
   console.log('listening on 8000');
 });
