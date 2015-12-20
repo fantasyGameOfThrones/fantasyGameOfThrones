4f2d0c4dd206e9ff1ef6b43e660d76fdc19ea99d
naomiajacobs <naomiajacobs@gmail.com>
2015-12-19 19:28:36 -0800

    Remove temporary test data and code; add action constants; begin formatting reducers

--- client/components/leftNav.jsx
+++ client/components/leftNav.jsx
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import actions from '../services/actionCreators.jsx';
 
 class LeftNav extends Component {
+  renderTabs(tabs) {
+    return tabs.map((tab, i) => {
+      return (
+        <li key={i} 
+            className="clickable"
+            onClick={this.navigate.bind(this,tab)}>
+          {tab}
+        </li>
+      );
+    });
+  }
   render() {
     const tabs = ['HOME','DRAFT','TRADE']
     return (
       <div id="left">
         <ul>
-          {tabs.map((tab, i)=><li key={i} onClick={this.navigate.bind(this, tab)}>{tab}</li>)}
+          {this.renderTabs(tabs)}
         </ul>
       </div>
     );
@@ -20,8 +31,8 @@ class LeftNav extends Component {
   
 };
 
-const select = ( state ) => {
+const select = (state) => {
   return {};
 };
 
-export default connect( select )( LeftNav );
+export default connect(select)(LeftNav);
