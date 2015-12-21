5b8044e0c79a87354a515d24a2b662134f1546d5
naomiajacobs <naomiajacobs@gmail.com>
2015-12-20 21:50:46 -0800

    Create test data to work with json-server

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -14,7 +14,19 @@ class Header extends Component {
   }
 
   testNetwork() {
-    this.props.dispatch(actions.updateUser(1,{email:'rjwholey@gmail.com',username:'ser_ryan'}));
+    this.props.dispatch(actions.updateUser(1,{
+      email:'rjwholey@gmail.com',
+      username:'ser_ryan',
+      leagueId: 1,
+      episodes: {
+        43: [1,2,3,4,5,6,7,8,9,10],
+        54: [1,2,3,4,5,6,7,8,9,10],
+        82: [1,2,3,4,5,6,7,8,9,10],
+        45: [1,2,3,4,5,6,7,8,9,10],
+        66: [1,2,3,4,5,6,7,8,9,10],
+        58: [1,2,3,4,5,6,7,8,9,10],
+      }
+    }));
   }
 
 };
--- testData.js
+++ testData.js
@@ -1,19 +1,53 @@
-export const user = {
-  id: 1,
-  username: 'kylecho',
-  email: 'kylecho.work@gmail.com',
-  leagueId: 1,
-  episodes: {
-    1: [1,2,3,4,5,6,7,8,9,10],
-    2: [1,2,3,4,5,6,7,8,9,10],
-    3: [1,2,3,4,5,6,7,8,9,10],
-    4: [1,2,3,4,5,6],
-    5: [1,2,3,4,5,6,7,8,9,10],
-    6: [7,8,9,10],
-  }
-};
+'use strict';
 
-export const league = {
+var fs = require('fs');
+
+const users = [
+  {
+    id: 1,
+    username: 'kylecho',
+    email: 'kylecho.work@gmail.com',
+    leagueId: 1,
+    episodes: {
+      1: [1,2,3,4,5,6,7,8,9,10],
+      2: [1,2,3,4,5,6,7,8,9,10],
+      3: [1,2,3,4,5,6,7,8,9,10],
+      4: [1,2,3,4,5,6],
+      5: [1,2,3,4,5,6,7,8,9,10],
+      6: [7,8,9,10],
+    }
+  },
+  {
+    id: 2,
+    username: 'naomijacobs',
+    email: 'naomiajacobs@gmail.com',
+    leagueId: 1,
+    episodes: {
+      7: [1,2,3,4,5,6,7,8,9,10],
+      8: [1,2,3,4,5,6,7,8,9,10],
+      9: [1,2,3,4,5,6,7,8,9,10],
+      10: [1,2,3,4,5,6],
+      11: [1,2,3,4,5,6,7,8,9,10],
+      12: [7,8,9,10],
+    }
+  },
+  {
+    id: 3,
+    username: 'wholigan',
+    email: 'wholey@gmail.com',
+    leagueId: 1,
+    episodes: {
+      13: [1,2,3,4,5,6,7,8,9,10],
+      14: [1,2,3,4,5,6,7,8,9,10],
+      15: [1,2,3,4,5,6,7,8,9,10],
+      16: [1,2,3,4,5,6],
+      17: [1,2,3,4,5,6,7,8,9,10],
+      18: [7,8,9,10],
+    }
+  }
+];
+
+const leagues = [{
   id: 1,
   name: 'TempestTargaryens',
   creatorId: 1,
@@ -59,23 +93,51 @@ export const league = {
       }
     }
   ]
+}];
+
+const characters = fs.readFileSync('./testData/characterData.txt', 'utf8', function(err, data) {
+  return data;
+})
+.split('\n')
+.map(function(char) {
+  var parts = char.split(',');
+  return {
+    id: parts[0],
+    name: parts[1].trim(),
+    nickname: parts[2] ? parts[2].trim() : null,
+    house: parts[3] ? parts[3].trim() : null,
+    imageUrl: parts[4].trim()
+  };
+});
+
+const events = fs.readFileSync('./testData/eventData.txt', 'utf8', function(err, data) {
+  return data;
+})
+.split('\n')
+.map(function(char) {
+  var parts = char.split(',');
+  return {
+    id: parts[0],
+    type: parts[1],
+    description: parts[2],
+    episodeId: parts[3],
+    characterId: parts[4],
+    points: parts[5]
+  };
+});
+
+const testData = {
+  login: {
+    token: 'abc123',
+    user: users[1],
+    league: leagues,
+    characters: characters,
+    events: events,
+  },
+  users: users,
+  leagues: leagues,
+  characters: characters,
+  events: events
 };
 
-export const characters = [];
-
-
-
-function addCharacter(id, name, nickname, house, imageUrl) {
-  characters.push({
-    id: id,
-    name: name,
-    nickname: nickname,
-    house: house,
-    imageUrl: imageUrl,
-  });
-}
-
-
-
-
-
+fs.writeFile('./dbTemplate.json', JSON.stringify(testData), function(err) {if (err) console.log(err);});
