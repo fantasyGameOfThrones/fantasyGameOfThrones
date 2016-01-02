5e70e9eaef4725935740f70e059b85267a0a443d
Brian Graham <graham.briank@gmail.com>
2016-01-02 15:58:16 -0800

    add delete query and select query for roster_data

--- server/db/helpersDB.js
+++ server/db/helpersDB.js
@@ -2,10 +2,29 @@ var mysql = require('mysql');
 var connection = require('./db');
 
 // ROSTER_DATA QUERIES
-// data = object with insert props. (user_id, char_id, league_id, episode num)
-exports.createNew = function (data) {
- var sql = mysql.format('INSERT INTO roster_data SET ?', [data]);
- return connection.queryAsync(sql);
+
+/*
+* function: createNew
+* arg: data: an object with 4 properties:
+*        user_id, char_id, league_id, episode num 
+* TODO: add error handling if data object is missing parameters needed to run query  
+*/
+exports.addCharacter = function (data) {
+  var sql = mysql.format('INSERT INTO roster_data SET ?', [data]);
+  return connection.queryAsync(sql);
+};
+
+exports.dropCharacter = function (data) {
+  var sql = mysql.format('DELETE FROM roster_data WHERE user_id = ?' +
+                         'AND char_id = ? AND league_id = ? AND episode = ?',
+                         [data.user_id, data.char_id, data.league_id, data.episode]);
+  return connection.queryAsync(sql);
+};
+
+// data object contains user_id and league_id
+exports.getRosterData = function (data) {
+  var sql = mysql.format('SELECT * FROM roster_data WHERE user_id = ? AND league_id = ?', [data.user_id, data.league_id])
+  return connection.queryAsync(sql);
 };
 
 
