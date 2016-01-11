c6bad74bfaee723b7e23468a4894b0215d06f7a2
naomiajacobs <naomiajacobs@gmail.com>
2016-01-11 14:08:36 -0800

    Add leagueCreation action

--- client/services/actionCreators/league.jsx
+++ client/services/actionCreators/league.jsx
@@ -1,3 +1,36 @@
 import network from '../network.jsx';
 import * as constants from '../actionConstants.jsx'
 
+const createLeagueSuccess = (body) => {
+  return {
+    type: constants.CREATE_LEAGUE_SUCCESS,
+    payload: {
+      league: body
+    }
+  };
+};
+
+const createLeagueFailure = (message) => {
+  return {
+    type: constants.CREATE_LEAGUE_FAILURE,
+    payload: {
+      error: message
+    }
+  };
+};
+
+export const createLeague = (name) => {
+  return (dispatch) => {
+    return network.leagueRequests('POST', null, {name})
+    .then((response) => {
+      if (!response.ok) {throw new Error('League creation failure: ', response)}
+      return response.json();
+    })
+    .then((body) => {
+      return dispatch(createLeagueSuccess(body));
+    })
+    .catch((error) => {
+      return dispatch(createLeagueFailure(error.message));
+    });
+  }
+};
