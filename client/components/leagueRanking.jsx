cb44f48dcda0f9bd6e78bad6ba2d132bbc6db893
naomiajacobs <naomiajacobs@gmail.com>
2016-01-11 12:22:47 -0800

    League rankings now works based on returned rosters

--- client/components/leagueRanking.jsx
+++ client/components/leagueRanking.jsx
@@ -3,41 +3,32 @@ import {connect} from 'react-redux';
 import store from '../services/store.jsx';
 
 class LeagueRanking extends Component {
-
-  render(){
+  
+  render() {
     return (
       <div>
-        <h1>HI</h1>
-      </div>
+        <ul>
+          {this.props.users.map((user, index) => {
+            return (
+              <li
+                key={index}
+                className='sideBarText'
+              >
+                {user.username}, points: {user.roster.points}
+              </li>
+            );
+          })}
+        </ul>
+      </div> 
     );
   }
-  
-  // render() {
-  //   return (
-  //     <div>
-  //       <ul>
-  //         {this.props.users.map((user, index) => {
-  //           return (
-  //             <li
-  //               key={index}
-  //               className='sideBarText'
-  //             >
-  //               {user.username}, points: {user.roster.points}
-  //             </li>
-  //           );
-  //         })}
-  //       </ul>
-  //     </div> 
-  //   );
-  // }
-
 };
 
 const select = (state) => {
   return {
-    // users: state.data.league.users.sort((a,b) => {
-    //   return a.roster.points < b.roster.points;
-    // }),
+    users: state.data.league.users.sort((a,b) => {
+      return a.roster.points < b.roster.points;
+    }),
   };
 };
 
