75af77f052ad632ec446b80609795788bf3c6df7
naomiajacobs <naomiajacobs@gmail.com>
2016-01-25 12:45:19 -0800

    Fix inconsistent spacing

--- client/components/newLeague.jsx
+++ client/components/newLeague.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import actions from '../services/actionCreators.jsx';
 
 class NewLeague extends Component {
